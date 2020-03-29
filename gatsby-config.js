431d9411e12afbbb8b4a54e26ab00ae750710a9f
michaelgt04 <20232897+michaelgt04@users.noreply.github.com>
2020-03-29 18:34:13 -0400

    Initial commit from gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Cooking blog`,
+    description: `Learn how to be a better home cook.`,
+    author: `Mike Thomson`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `cooking-site`,
+        short_name: `cooking`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
