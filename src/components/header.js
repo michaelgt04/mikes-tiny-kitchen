50bc8e8fa72629f5df917a0135f1efc49641073e
michaelgt04 <20232897+michaelgt04@users.noreply.github.com>
2020-04-03 20:46:17 -0400

    Clean up fonts and home page

--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,9 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header className="container m-auto">
-    <div className="flex justify-center py-8">
-      <h1>
+  <header className="container max-w-2xl m-auto">
+    <div className="flex px-6 md:px-0 py-8">
+      <h1 className="pr-2 md:pr-4 border-r-2 font-bold">
         <Link
           to="/"
           style={{
@@ -15,6 +15,12 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
+      <Link className="ml-2 md:ml-4 font-display underline" to="/about">
+        About
+      </Link>
+      <Link className="ml-4 md:ml-6 font-display underline" to="/blog">
+        Blog
+      </Link>
     </div>
   </header>
 )
---
+++ src/constants/images.js
@@ -0,0 +1,4 @@
+export const BREAKFAST_SANDWICH_IMG =
+  "https://res.cloudinary.com/duor0bzmo/image/upload/c_scale,q_100,w_512/v1585866139/mikestinykitchen/breakfast-sandwich.jpg"
+export const CINNAMON_ROLL_IMG =
+  "https://res.cloudinary.com/duor0bzmo/image/upload/c_scale,w_512/a_90/v1585866153/mikestinykitchen/cinnamon-rolls.jpg"
