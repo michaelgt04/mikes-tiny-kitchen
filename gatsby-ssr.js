123be58356cff450aab40ee71970da77d27e1914
michaelgt04 <20232897+michaelgt04@users.noreply.github.com>
2020-05-01 12:25:53 -0400

    Put ssr config back

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,7 @@
-import React from "react"
-export const onRenderBody = ({ setPostBodyComponents }) => {
-  return setPostBodyComponents([
-    <script src="https://f.convertkit.com/ckjs/ck.5.js" />,
-  ])
-}
+/**
+ * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/ssr-apis/
+ */
+
+// You can delete this file if you're not using it
